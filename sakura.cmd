cb8ad35c5545c812d4695a6150bb8f6e3fe9de37
Paulo Trevizan <ptrevizan@gmail.com>
2021-08-06 23:56:21 -0300

    Add files via upload

---
+++ sakura.cmd
@@ -0,0 +1,945 @@
+;------------------------|
+; Arquivo CMD - Sakura   |
+; criado por Ixnaydk     |
+;------------------------|
+[Remap]
+x = x
+y = y
+z = z
+a = a
+b = b
+c = c
+s = s
+
+[Defaults]
+command.time = 15
+command.buffer.time = 1
+
+[Command]
+name = "shungokusatsu"
+command = x,x,F,a,z
+time = 45
+
+[Command]
+name = "kassethadou"
+command = ~D,DF,F,DF,D,DB,B,x
+time = 30
+
+[Command]
+name = "seojhoakim"
+command = ~D,DB,B,DB,D,DF,F,y
+time = 30
+
+[Command]
+name = "kenmatoobozo"
+command = ~B,DB,D,DF,F,B,z
+time = 30
+
+[Command]
+name = "tonmanuku_x"
+command = ~D,DF,F,D,DF,F,x
+time = 25
+
+[Command]
+name = "tonmanuku_y"
+command = ~D,DF,F,D,DF,F,y
+time = 25
+
+[Command]
+name = "tonmanuku_z"
+command = ~D,DF,F,D,DF,F,z
+time = 25
+
+[Command]
+name = "midarezakura_a"
+command = ~D,DF,F,D,DF,F,a
+time = 25
+
+[Command]
+name = "midarezakura_b"
+command = ~D,DF,F,D,DF,F,b
+time = 25
+
+[Command]
+name = "midarezakura_c"
+command = ~D,DF,F,D,DF,F,c
+time = 25
+
+[Command]
+name = "haruichiban_a"
+command = ~D,DB,B,D,DB,B,a
+time = 25
+
+[Command]
+name = "haruichiban_b"
+command = ~D,DB,B,D,DB,B,b
+time = 25
+
+[Command]
+name = "haruichiban_c"
+command = ~D,DB,B,D,DB,B,c
+time = 25
+
+[Command]
+name = "shinkuuhadou_x"
+command = ~D,DB,B,D,DB,B,x
+time = 25
+
+[Command]
+name = "shinkuuhadou_y"
+command = ~D,DB,B,D,DB,B,y
+time = 25
+
+[Command]
+name = "shinkuuhadou_z"
+command = ~D,DB,B,D,DB,B,z
+time = 25
+
+[Command]
+name = "buusetatsumaki_a"
+command = ~F,DF,D,DB,B,a
+time = 30
+
+[Command]
+name = "buusetatsumaki_b"
+command = ~F,DF,D,DB,B,b
+time = 30
+
+[Command]
+name = "buusetatsumaki_c"
+command = ~F,DF,D,DB,B,c
+time = 30
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,x+y
+time = 20
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,x+z
+time = 20
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,y+z
+time = 20
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,a+b
+time = 20
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,a+c
+time = 20
+
+[Command]
+name = "asurawarpfwd"
+command = F,D,DF,b+c
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,x+y
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,x+z
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,y+z
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,a+b
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,a+c
+time = 20
+
+[Command]
+name = "asurawarpback"
+command = B,D,DB,b+c
+time = 20
+
+[Command]
+name = "shoryuken_x"
+command = ~F,D,DF,x
+time = 20
+
+[Command]
+name = "shoryuken_y"
+command = ~F,D,DF,y
+time = 20
+
+[Command]
+name = "shoryuken_z"
+command = ~F,D,DF,z
+time = 20
+
+[Command]
+name = "rasetsu_a"
+command = ~F,D,DF,a
+time = 20
+
+[Command]
+name = "rasetsu_b"
+command = ~F,D,DF,b
+time = 20
+
+[Command]
+name = "rasetsu_c"
+command = ~F,D,DF,c
+time = 20
+
+[Command]
+name = "tenmakuujinkyaku_a"
+command = ~D,DF,F,a
+time = 20
+
+[Command]
+name = "tenmakuujinkyaku_b"
+command = ~D,DF,F,b
+time = 20
+
+[Command]
+name = "tenmakuujinkyaku_c"
+command = ~D,DF,F,c
+time = 20
+
+[Command]
+name = "zatsuknuckle_x"
+command = ~D,DB,B,x
+time = 20
+
+[Command]
+name = "zatsuknuckle_y"
+command = ~D,DB,B,y
+time = 20
+
+[Command]
+name = "zatsuknuckle_z"
+command = ~D,DB,B,z
+time = 20
+
+[Command]
+name = "hadouken_x"
+command = ~D,DF,F,x
+time = 20
+
+[Command]
+name = "hadouken_y"
+command = ~D,DF,F,y
+time = 20
+
+[Command]
+name = "hadouken_z"
+command = ~D,DF,F,z
+time = 20
+
+[Command]
+name = "tatsumaki_a"
+command = ~D,DB,B,a
+time = 20
+
+[Command]
+name = "tatsumaki_b"
+command = ~D,DB,B,b
+time = 20
+
+[Command]
+name = "tatsumaki_c"
+command = ~D,DB,B,c
+time = 20
+
+[Command]
+name = "customcombo"
+command = x+b+z
+time = 1
+
+[Command]
+name = "transformacao"
+command = a+y+c
+time = 1
+
+[Command]
+name = "FF"     
+command = F,F
+time = 10
+
+[Command]
+name = "BB"     
+command = B,B
+time = 10
+
+[Command]
+name = "alphacounter"
+command = x+a
+time = 1
+
+[Command]
+name = "betacounter"
+command = y+b
+time = 1
+
+[Command]
+name = "gammacounter"
+command = z+c
+time = 1
+
+[Command]
+name = "recovery"
+command = x+y
+time = 1
+
+[Command]
+name = "down_a"
+command = /$D,a
+time = 1
+
+[Command]
+name = "down_b"
+command = /$D,b
+time = 1
+
+[Command]
+name = "powercharge"
+command = /s
+time = 1
+
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+
+[Command]
+name = "holdup" 
+command = /$U
+time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+
+[Statedef -1]
+;----------------|
+;IA - Comandos   |
+;----------------|
+[State -1, Defesa]
+type = ChangeState
+value = 130
+triggerall = (Var(49) > 0 && RoundState = 2 && Random < 890 && Ctrl)
+trigger1 = (StateType = S && EnemyNear,MoveType = A && Ctrl)
+
+[State -1, Defesa Abaixado]
+type = ChangeState
+value = 131
+triggerall = (Var(49) > 0 && RoundState = 2 && Random < 890 && Ctrl)
+trigger1 = (StateType = C && EnemyNear,MoveType = A && Ctrl)
+
+[State -1, Defesa Aérea]
+type = ChangeState
+value = 132
+triggerall = (Var(49) > 0 && RoundState = 2 && Random < 890 && Ctrl)
+trigger1 = (StateType = A && EnemyNear,MoveType = A && Ctrl)
+
+;------------------|
+;Shun Goku Satsu   |
+;------------------|
+[State -1, Shun Goku Satsu]
+type = ChangeState
+value = 3005
+triggerall = (Power>=3000&&Var(10)=1)
+triggerall = Command = "shungokusatsu"
+triggerall = StateType = S
+trigger1 = Ctrl
+trigger2 = StateNo=[200,240]
+
+;------------------|
+;Kasset Hadouken   |
+;------------------|
+[State -1, Kasset Hadouken]
+type = ChangeState
+value = 3070
+triggerall = Power >= 3000
+triggerall = Command = "kassethadou"
+triggerall = StateType = S
+trigger1 = StateType != A
+trigger1 = Ctrl
+
+;---------------|
+;Seo Jhoa Kim   |
+;---------------|
+[State -1, Seo Jhoa Kim]
+type = ChangeState
+value = 3080
+triggerall = Power >= 3000
+triggerall = Command = "seojhoakim"
+triggerall = StateType = S
+trigger1 = StateType != A
+trigger1 = Ctrl
+
+;----------------|
+;Kenmat Oobozo   |
+;----------------|
+[State -1, Kenmat Oobozo]
+type = ChangeState
+value = 3095
+triggerall = Power >= 3000
+triggerall = Command = "kenmatoobozo"
+triggerall = (!NumHelper(1025)&&!NumHelper(1026)&&!NumHelper(3015))
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;------------|
+;Tonmanuku   |
+;------------|
+[State -1, Tonmanuku]
+type = ChangeState
+value = 3090
+triggerall = Var(10) = 1
+triggerall = (((Command = "tonmanuku_x" || Command = "tonmanuku_y" || Command = "tonmanuku_z") && Power >= 1000) || ((Command = "tonmanuku_y" || Command = "tonmanuku_z") && Power >= 2000) || (Command = "tonmanuku_z" && Power >= 3000))
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;----------------|
+;Midare Zakura   |
+;----------------|
+[State -1, Midare Zakura]
+type = ChangeState
+value = 3039
+triggerall = (((Command = "midarezakura_a" || Command = "midarezakura_b" || Command = "midarezakura_c") && Power >= 1000) || ((Command = "midarezakura_b" || Command = "midarezakura_c") && Power >= 2000) || (Command = "midarezakura_c" && Power >= 3000))
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;---------------|
+;Haru Ichiban   |
+;---------------|
+[State -1, Haru Ichiban]
+type = ChangeState
+value = 3030
+triggerall = (((Command = "haruichiban_a" || Command = "haruichiban_b" || Command = "haruichiban_c") && Power >= 1000) || ((Command = "haruichiban_b" || Command = "haruichiban_c") && Power >= 2000) || (Command = "haruichiban_c" && Power >= 3000))
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;-------------------|
+;Shinkuu Hadouken   |
+;-------------------|
+[State -1, Shinkuu Hadouken]
+type = ChangeState
+value = ifelse(Var(10) = 1,3060,3010)
+triggerall = (((Command = "shinkuuhadou_x" || Command = "shinkuuhadou_y" || Command = "shinkuuhadou_z") && Power >= 1000) || ((Command = "shinkuuhadou_y" || Command = "shinkuuhadou_z") && Power >= 2000) || (Command = "shinkuuhadou_z" && Power >= 3000))
+triggerall = StateType != A
+triggerall = (!NumHelper(1025)&&!NumHelper(1026)&&!NumHelper(3015))
+trigger1 = Ctrl
+
+;--------------------------|
+;Buuse Tatsumaki - Fraco   |
+;--------------------------|
+[State -1, Buuse Tatsumaki Fraco]
+type = ChangeState
+value = 3000
+triggerall = Power >= 1000
+triggerall = (Command = "buusetatsumaki_a" || Command = "buusetatsumaki_b" || Command = "buusetatsumaki_a")
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;--------------------------------|
+;Buuse Tatsumaki - Médio/Forte   |
+;--------------------------------|
+[State -1, Buuse Tatsumaki Médio/Forte]
+type = ChangeState
+value = 3020
+triggerall = (((Command = "buusetatsumaki_b" || Command = "buusetatsumaki_c") && Power >= 2000) || (Command = "buusetatsumaki_c" && Power >= 3000))
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;-------------|
+;Asura Warp   |
+;-------------|
+[State -1, Asura Warp]
+type = ChangeState
+value = 1060
+triggerall = Var(10) = 1
+triggerall = (Command = "asurawarpfwd" || Command = "asurawarpback")
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;--------------------|
+;Shoryuken - Fraco   |
+;--------------------|
+[State -1, Shoryuken Fraco]
+type = ChangeState
+value = 1010
+triggerall = Command = "shoryuken_x"
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;--------------------|
+;Shoryuken - Médio   |
+;--------------------|
+[State -1, Shoryuken Médio]
+type = ChangeState
+value = 1011
+triggerall = Command = "shoryuken_y"
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;--------------------|
+;Shoryuken - Forte   |
+;--------------------|
+[State -1, Shoryuken Forte]
+type = ChangeState
+value = 1012
+triggerall = Command = "shoryuken_z"
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;----------------|
+;Rasetsu Kyaku   |
+;----------------|
+[State -1, Rasetsu Kyaku]
+type = ChangeState
+value = 1050
+triggerall = Var(10) = 1
+triggerall = (Command = "rasetsu_a" || Command = "rasetsu_b" || Command = "rasetsu_c")
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;---------------------|
+;Tenma Kuujin Kyaku   |
+;---------------------|
+[State -1, Tenma Kuujin Kyaku]
+type = ChangeState
+value = 1100
+triggerall = Var(10) = 1
+triggerall = StateType = A
+triggerall = (Command="tenmakuujinkyaku_a"||Command="tenmakuujinkyaku_b"||Command="tenmakuujinkyaku_c")
+trigger1 = Vel X !=0
+trigger1 = Ctrl
+
+;-------------------------|
+;Tatsumaki Senpuu Kyaku   |
+;-------------------------|
+[State -1, Tatsumaki Senpuu Kyaku]
+type = ChangeState
+value = 1000
+triggerall = (Command="tatsumaki_a"||Command="tatsumaki_b"||Command="tatsumaki_c")
+triggerall = StateType != A
+trigger1 = Ctrl
+
+;----------------|
+;Zatsu Knuckle   |
+;----------------|
+[State -1, Zatsu Knuckle]
+type = ChangeState
+value = 1080
+triggerall = Var(10) = 1
+triggerall = (Command="zatsuknuckle_x"||Command="zatsuknuckle_y"||Command="zatsuknuckle_z")
+triggerall = StateType = S
+trigger1 = StateType != A
+trigger1 = Ctrl
+
+;-----------|
+;Hadouken   |
+;-----------|
+[State -1, Hadouken]
+type = ChangeState
+value = 1020
+triggerall = (Command="hadouken_x"||Command="hadouken_y"||Command="hadouken_z")
+triggerall = StateType != A
+triggerall = (!NumHelper(1025)&&!NumHelper(1026)&&!NumHelper(3015))
+trigger1 = Ctrl
+
+;---------------|
+;Custom Combo   |
+;---------------|
+[State -1, Custom Combo]
+type = ChangeState
+value = 10014
+triggerall = Var(11) = 0
+triggerall = Var(10) = 0
+triggerall = Command = "customcombo"
+triggerall = StateType != A
+trigger1 = StateType = S
+trigger1 = Ctrl
+
+;----------------|
+;Transformação   |
+;----------------|
+[State -1, Transformação]
+type = ChangeState
+value = 10012
+triggerall = Var(10) = 0
+triggerall = Var(11) = 0
+triggerall = Command = "transformacao"
+triggerall = StateType = S
+trigger1 = StateType != A
+trigger1 = Ctrl
+
+;---------|
+;Gancho   |
+;---------|
+[State -1, Gancho]
+type = ChangeState
+value = 220
+triggerall = Var(14) > 0
+triggerall = Command = "z"
+triggerall = Command = "holdfwd" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = Ctrl
+trigger2 = (StateNo = 215&&MoveContact)
+
+;---------|
+;Correr   |
+;---------|
+[State -1, Correr]
+type = ChangeState
+value = 100
+triggerall = command = "FF"
+trigger1 = StateType = S
+trigger1 = ctrl
+
+;----------------|
+;Pulo Pra Tras   |
+;----------------|
+[State -1, Pulo Pra Tras]
+type = ChangeState
+value = 105
+triggerall = Command = "BB"
+trigger1 = StateType = S
+trigger1 = ctrl
+
+;----------------|
+;Alpha Counter   |
+;----------------|
+[State -1, Alpha Counter]
+type = ChangeState
+value = 1065
+triggerall = Power >= 500
+triggerall = Command = "alphacounter"
+triggerall = StateType != A
+triggerall = (StateNo >= 150 && StateNo <= 152)
+triggerall = EnemyNear,MoveType = A
+trigger1 = EnemyNear,HitDefAttr = SCA,NA,SA,HA
+trigger1 = StateType = S
+
+;---------------|
+;Beta Counter   |
+;---------------|
+[State -1, Beta Counter]
+type = ChangeState
+value = 1070
+triggerall = Power >= 500
+triggerall = Command = "betacounter"
+triggerall = StateType != A
+triggerall = (StateNo >= 150 && StateNo <= 152)
+triggerall = EnemyNear,MoveType = A
+trigger1 = EnemyNear,HitDefAttr = SCA,NA,SA,HA
+trigger1 = StateType = S
+
+;----------------|
+;Gamma Counter   |
+;----------------|
+[State -1, Gamma Counter]
+type = ChangeState
+value = 1075
+triggerall = Power >= 500
+triggerall = Command = "gammacounter"
+triggerall = StateType != A
+triggerall = (StateNo >= 150 && StateNo <= 152)
+triggerall = EnemyNear,MoveType = A
+trigger1 = EnemyNear,HitDefAttr = SCA,NA,SA,HA
+trigger1 = StateType = S
+
+;---------------|
+;Power Charge   |
+;---------------|
+[State -1, Power Charge]
+type = ChangeState
+value = 700
+triggerall = (Command="powercharge"&&Power<3000)
+triggerall = StateType = S
+trigger1 = Ctrl
+
+;-------------|
+;Soco Fraco   |
+;-------------|
+[State -1, Soco Fraco]
+type = ChangeState
+value = 200
+triggerall = Command = "x" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = P2BodyDist X >= 10
+trigger1 = Ctrl
+trigger2 = (StateNo=400&&MoveContact)
+trigger3 = (Var(11)!=0&&MoveContact&&StateType!=A)
+
+;-------------------|
+;Soco Fraco Perto   |
+;-------------------|
+[State -1, Soco Fraco Perto]
+type = ChangeState
+value = 205
+triggerall = Command = "x" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = P2BodyDist X < 10
+trigger1 = Ctrl
+
+;-------------|
+;Soco Médio   |
+;-------------|
+[State -1, Soco Médio]
+type = ChangeState
+value = 210
+triggerall = Command = "y" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = Ctrl
+trigger2 = ((StateNo=200||StateNo=205)&&MoveContact)
+
+;-------------|
+;Soco Forte   |
+;-------------|
+[State -1, Soco Forte]
+type = ChangeState
+value = 215
+triggerall = Command = "z" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = Ctrl
+trigger2 = (StateNo=210&&MoveContact)
+trigger3 = (Var(11)!=0&&MoveContact&&StateType!=A)
+
+;--------------|
+;Chute Fraco   |
+;--------------|
+[State -1, Chute Fraco]
+type = ChangeState
+value = 230
+triggerall = Command = "a" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = P2BodyDist X >= 20
+trigger1 = Ctrl
+
+;--------------------|
+;Chute Fraco Perto   |
+;--------------------|
+[State -1, Chute Fraco Perto]
+type = ChangeState
+value = 225
+triggerall = (Command = "a" || Command = "b")
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = P2BodyDist X < 20
+trigger1 = Ctrl
+
+;--------------|
+;Chute Médio   |
+;--------------|
+[State -1, Chute Médio]
+type = ChangeState
+value = 235
+triggerall = Command = "b" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = P2BodyDist X >= 20
+trigger1 = Ctrl
+
+;--------------|
+;Chute Forte   |
+;--------------|
+[State -1, Chute Forte]
+type = ChangeState
+value = 240
+triggerall = Command = "c" 
+triggerall = Command != "holddown"
+triggerall = StateType = S
+trigger1 = Ctrl
+
+;----------------------|
+;Soco Fraco Abaixado   |
+;----------------------|
+[State -1, Soco Fraco Abaixado]
+type = ChangeState
+value = 400
+triggerall = Command = "x" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+
+;----------------------|
+;Soco Médio Abaixado   |
+;----------------------|
+[State -1, Soco Médio Abaixado]
+type = ChangeState
+value = 405
+triggerall = Command = "y" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+trigger2 = (StateNo = 400&&MoveContact)
+
+;----------------------|
+;Soco Forte Abaixado   |
+;----------------------|
+[State -1, Soco Forte Abaixado]
+type = ChangeState
+value = 410
+triggerall = Command = "z" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+trigger2 = (StateNo = 405&&MoveContact)
+
+;-----------------------|
+;Chute Fraco Abaixado   |
+;-----------------------|
+[State -1, Chute Fraco Abaixado]
+type = ChangeState
+value = 415
+triggerall = Command = "a" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+
+;-----------------------|
+;Chute Médio Abaixado   |
+;-----------------------|
+[State -1, Chute Médio Abaixado]
+type = ChangeState
+value = 420
+triggerall = Command = "b" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+
+;-----------------------|
+;Chute Forte Abaixado   |
+;-----------------------|
+[State -1, Chute Forte Abaixado]
+type = ChangeState
+value = 425
+triggerall = Command = "c" 
+triggerall = Command = "holddown"
+triggerall = StateType = C
+trigger1 = Ctrl
+
+;-------------------|
+;Soco Fraco Aéreo   |
+;-------------------|
+[State -1, Soco Fraco Aéreo]
+type = ChangeState
+value = 600
+triggerall = Command = "x" 
+triggerall = Command != "holddown" 
+trigger1 = StateType = A
+trigger1 = Ctrl
+trigger2 = (StateNo = 615&&MoveContact)
+
+;-------------------|
+;Soco Médio Aéreo   |
+;-------------------|
+[State -1, Soco Médio Aéreo]
+type = ChangeState
+value = 605
+triggerall = Command = "y" 
+triggerall = Command != "holddown"
+trigger1 = StateType = A
+trigger1 = Ctrl
+trigger2 = (StateNo = 600&&MoveContact)
+
+;-------------------|
+;Soco Forte Aéreo   |
+;-------------------|
+[State -1, Soco Forte Aéreo]
+type = ChangeState
+value = 610
+triggerall = Command = "z" 
+triggerall = Command != "holddown"
+trigger1 = StateType = A
+trigger1 = Ctrl
+trigger2 = (StateNo = 605&&MoveContact)
+
+;--------------------|
+;Chute Fraco Aéreo   |
+;--------------------|
+[State -1, Chute Fraco Aéreo]
+type = ChangeState
+value = 615
+triggerall = Command = "a" 
+triggerall = Command != "holddown"
+trigger1 = StateType = A
+trigger1 = Ctrl
+
+;--------------------|
+;Chute Médio Aéreo   |
+;--------------------|
+[State -1, Chute Médio Aéreo]
+type = ChangeState
+value = 620
+triggerall = Command = "b" 
+triggerall = Command != "holddown"
+trigger1 = StateType = A
+trigger1 = Ctrl
+trigger2 = (StateNo = 615&&MoveContact)
+
+;--------------------|
+;Chute Forte Aéreo   |
+;--------------------|
+[State -1, Chute Forte Aéreo]
+type = ChangeState
+value = 625
+triggerall = Command = "c" 
+triggerall = Command != "holddown"
+trigger1 = StateType = A
+trigger1 = Ctrl
+trigger2 = (StateNo = 620&&MoveContact)
